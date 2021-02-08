9b3fe31e63b8ae45d52523bf351161101a30b943
Nikita <nikita199801@gmail.com>
2021-02-08 22:51:26 +0300

    Added button logic

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,7 +10,7 @@ export default class Dashboard extends React.Component{
             todos: data
         }
     }
-
+    
 
     render(){
         const todos =this.state.todos.map((todo) =>{
--- src/components/Plate.js
+++ src/components/Plate.js
@@ -1,18 +1,19 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Paper from '@material-ui/core/Paper';
 import Checkbox from '@material-ui/core/Checkbox';
 import "../styles/Plate.css"
 import MyButton from "./MyButton"
-
 export default function Plate(props){
+    const [canDelete, onDelete] = useState(!props.isDone)
     return(
         <div className="plate">
             <span className="plate-text-content">
                 {props.content}
             </span>
             <span className="control-section">
-                <Checkbox name="Done" checked = {props.isDone}/>
-                <button>Delete</button>
+                {/* <input type="checkbox" checked = {props.isDone}></input> */}
+                <Checkbox name="Done" defaultChecked = {props.isDone} onChange={()=>{onDelete(!canDelete)}}/>
+                <button disabled = {canDelete}>Delete</button>
                 <button>Edit</button>
             </span>
         </div>       
