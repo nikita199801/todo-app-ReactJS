ac0036bb93f2c7958ae574794d2f99d0c4f65f9f
Nikita <nikita199801@gmail.com>
2021-02-16 17:47:08 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const fs = require('fs')
 
-http.createServer((req, res)=>{
+http.createServer((req, res)=>{ 
     switch(req.method){
         case "POST":{
             if (req.url === '/new') {
